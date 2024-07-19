16c6bb9e4134ab09c395e67e11d9464c692f3f08
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-19 17:05:03 +0900

    done

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 //- 검색이 가능하다
 //- pagenation이 있다.
 //- 반응형 웹페이지다.
+const url = new URL(``);
+const serch_text_box= document.getElementById(" serch_text_box");
+const serch_text_button= document.getElementById(" serch_text_button");
+const getNews = async () => {
+    try {
+        const response = await fetch(url);
+            console.log(url)
+    } catch (error) {
+        errorRender(error.message);
+    }
+};
