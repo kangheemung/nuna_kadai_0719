5350a67b37ae31d493ebb04102dbba5743f247d4
kurotamashi30 <kurotamashi30@gmail.com>
2024-07-21 21:59:53 +0900

    test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-    const API_KEY = "#";
-    const city = "seoul";
+    const API_KEY = "3c13cc157319649e269e65600cd04117";
+    let city = "";
     const catnum = 200;
     const lang = "kr"; // 언어 설정 추가
     const url = new URL(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang={lang}&appid=${API_KEY}`);
@@ -11,12 +11,21 @@
     const hamburgers = document.querySelectorAll(".hamburger"); // 複数の要素を取得するためquerySelectorAllを使用// 複数の要素を取得するためquerySelectorAllを使用
     const sidebar = document.querySelector(".sidebar");
     const close = document.getElementById("close");
+    const citySelect = document.getElementById("inputGroupSelect03");
     const SeoulNowtemp = document.getElementById("SeoulNowtemp");
     const SeoulLowtemp = document.getElementById("SeoulLowtemp");
     const SeoulHightemp= document.getElementById("SeoulHightemp");
+    const weathericonUrl= document.getElementById("weather_img");
 
     // sidebar要素を取得する
     const head_top = document.querySelector(".head_top");
+    citySelect.addEventListener("change", function() {
+      city = this.value.toLowerCase();// 선택한 도시명으로 city 변수 업데이트
+        console.log("Selected City:", city); // 선택한 도시명을 확인
+        // 여기서 getNews 함수를 호출하여 선택한 도시의 날씨 정보를 가져올 수 있습니다
+        url.href = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&appid=${API_KEY}`;
+    getNews();
+    });
 //検索
         const getKeyword = async () => {
             let keyword = searchTextBox.value.trim();
@@ -76,10 +85,20 @@ function kelvinToCelsius(kelvin) { //섭씨 온도 계산
 const celsiusTemp = kelvinToCelsius(kelvinTemp); // 섭씨로 변환된 온도 값
 const Hightemp= kelvinToCelsius(data.main.temp_min);
 const celsiusTempMin = kelvinToCelsius(data.main.temp_max);
+const iconUrl = '<img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png" alt="' + data.weather[0].description + '"/>';
+
+// 이미지를 SeoulIcon 요소에 삽입
 
 SeoulNowtemp.innerText = `현재온도는:  ${celsiusTemp.toFixed(2)}°C`; // data.main.temp로 현재 기온을 가져올 수 있음
 
+const weathericonUrl =
+'<img src= "http://openweathermap.org/img/wn/'
++ WeatherResult.weather[0].icon +
+'.png" alt="' + WeatherResult.weather[0].description + '"/>'
+
+$('.SeoulIcon').html(weathericonUrl);
 SeoulIcon.innerText= `${city}의 날씨는?`;
+
 
 SeoulLowtemp.innerText = `최저 기온은 ${celsiusTempMin.toFixed(2)}°C 입니다.`;
 
@@ -103,12 +122,10 @@ console.log("기압은 ",data["main"]["pressure"],"입니다.")
 console.log("풍향은 ",data["wind"]["deg"],"입니다.")
 //  풍속 : wind - speed
 console.log("풍속은 ",data["wind"]["speed"],"입니다.")
-
-            } catch (error) {
+} catch (error) {
                 console.error(error.message);
             }
         };
 
 
-        // Call functions here or add more logic as needed
         getNews();
